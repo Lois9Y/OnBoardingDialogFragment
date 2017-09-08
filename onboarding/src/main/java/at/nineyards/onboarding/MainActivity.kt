42eeb1d82c11dfbbf7f725eac6a93d862c3b77d8
Alois Wollersberger <alois.wollersberger@9yards.at>
2017-09-08 10:47:36 +0200

    initial commit

---
+++ onboarding/src/main/java/at/nineyards/onboarding/MainActivity.kt
@@ -0,0 +1,15 @@
+package at.nineyards.onboarding
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Lois-9Y on 08/09/2017.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
